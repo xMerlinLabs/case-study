0b87ce813c4a576247bcef0171a5b4cf8203565d
Nathan Schwartz <nathan.schwartz95@gmail.com>
2020-01-27 17:58:20 -0600

    Server hardening, favicon, and npm compatibility

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Provides some basic server hardening
+app.use(helmet());
 
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
